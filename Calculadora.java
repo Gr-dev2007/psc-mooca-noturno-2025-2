a835621caaf40b747fcac2b877238aef28271553
Danilo de Souza Miguel <68231382+Danilo-Miguel@users.noreply.github.com>
2025-09-30 20:20:09 -0300

    Sobrecarga

--- Calculadora.java
+++ Calculadora.java
@@ -1,29 +1,40 @@
 public class Calculadora {
 
-    public int soma(int a, int b){
-        int resultado;
-        resultado = a + b;
-        return resultado;
+    // public int soma(int a, int b){
+    //     int resultado =  a +b;
+    //     return resultado;
         
+    // }
+
+    public double soma(double a , double b){
+               double resultado = a + b;
+                return resultado;
+
+    }
+
+      public double soma(double a , double b, double c){
+               double resultado = a + b + c;
+                return resultado;
+
     }
 
 
-     public int subtracao(int a, int b){
-        int resultado;
+     public double  subtracao(double a, double b){
+        double resultado;
         resultado = a - b;
         return resultado;
         
     }
 
-    public int multiplicao(int a, int b){
-        int resultado;
+    public double multiplicacao(double a, double b){
+        double resultado;
         resultado = a * b;
         return resultado;
         
     }
 
-    public int divisao(int a, int b){
-        int resultado;
+    public double divisao(double a, double b){
+        double resultado;
         resultado = a / b;
         return resultado;
         
---
+++ TestCalculadora.java
@@ -0,0 +1,48 @@
+
+import javax.swing.JOptionPane;
+
+public class TestCalculadora {
+    public static void main(String[] args) {
+
+        Calculadora calc = new Calculadora();
+
+        int op = Integer.parseInt(JOptionPane.showInputDialog("1-Soma(2 números)\n2-Soma(3 números)\n 3-Subtração\n4-Multiplicação\n5-Divisão"));
+
+        double operando1 = Double.parseDouble(JOptionPane.showInputDialog("Qual é o primeiro operando"));
+        double operando2 = Double.parseDouble(JOptionPane.showInputDialog("Qual é o segundo operando"));
+        double operando3 = Double.parseDouble(JOptionPane.showInputDialog("Qual é o terceiro operando"));
+         
+         double resultado;
+
+         if(op == 1){
+            resultado = calc.soma(operando1, operando2);
+            JOptionPane.showMessageDialog(null, "O resultado da soma é " + resultado);
+         }
+
+         else if(op == 2){
+            resultado = calc.soma(operando1, operando2, operando3);
+            JOptionPane.showMessageDialog(null, "O resultado da soma é " + resultado);
+         }
+         else if (op == 3){
+            resultado = calc.subtracao(operando1, operando2);
+            JOptionPane.showMessageDialog(null, "O resultado da subtração é " + resultado);
+           
+         }
+          else if (op == 4){
+            resultado = calc.multiplicacao(operando1, operando2);
+            JOptionPane.showMessageDialog(null, "O resultado da multiplicação é " + resultado);
+         }
+          else if (op == 5){
+            resultado = calc.divisao(operando1, operando2);
+            JOptionPane.showMessageDialog(null, "O resultado da divisão é " + resultado);
+         
+         }
+         else{
+            JOptionPane.showMessageDialog(null, "Opção s");
+         }
+
+        
+    }
+
+    
+}
